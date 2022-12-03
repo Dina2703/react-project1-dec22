8950c65f6429b3ac1ec1e2f31fc6ca37666c734e
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-12-03 15:37:42 -0500

    useState hook practice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 // import Props from "./components/TimeOfDay";
 // import Cats from "./components/cats-profiles/Cats";
 // import AirbnbPage from "./components/airbnb/AirbnbPage";
+
+import { useState } from "react";
+
 // import TravelJournal from "./components/travel-journal/TravelJournal";
-
-import MemeHome from "./components/meme-generator/MemeHome";
-
+// import MemeHome from "./components/meme-generator/MemeHome";
 function App() {
+  const [itemArr, setItemArr] = useState([]);
+
+  const addItem = () => {
+    let newItem = `item ${itemArr.length + 1}`;
+    setItemArr((prev) => [...prev, newItem]);
+    console.log(newItem);
+  };
+
+  const itemEl = itemArr.map((item) => <p key={item}>{item}</p>);
+
+  //challenge
+
+  function greeting(name) {
+    let hours = new Date().getHours();
+
+    let answer;
+
+    if (hours >= 4 && hours < 12) {
+      answer = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      answer = "afternoon";
+    } else if (hours >= 17 && hours < 20) {
+      answer = "evening";
+    } else {
+      answer = "night";
+    }
+
+    return `Good ${answer} ${name}`;
+  }
+
+  console.log(greeting("Sam"));
+
   return (
     <div className="App">
+      <button onClick={addItem}>Add Item</button>
+      {itemEl}
+
       {/*
       <Props />
       <Cats />
       <TravelJournal />
       <AirbnbPage />
-    */}
       <MemeHome />
+    */}
     </div>
   );
 }
--- src/components/meme-generator/MemeForm.js
+++ src/components/meme-generator/MemeForm.js
@@ -1,7 +1,43 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { BsCardImage } from "react-icons/bs";
+import axios from "axios";
 
 function MemeForm() {
+  const [memes, setMemes] = useState(null);
+  const [randomMeme, setRandomMeme] = useState(null);
+  // console.log(memes);
+  console.log(randomMeme);
+
+  //fetch data from the Api
+  const getAllMemes = () => {
+    axios
+      .get("https://api.imgflip.com/get_memes")
+      .then((response) => {
+        const memesData = response.data;
+        // console.log(memesData);
+        setMemes(memesData.data.memes);
+      })
+      .catch((err) => console.log(`Error: ${err}`));
+  };
+
+  //fetch data when page loaded
+  useEffect(() => {
+    getAllMemes();
+  }, []);
+
+  //get random meme from the array of memes
+  function getRandomMeme() {
+    const randomMemeId = Math.floor(Math.random() * memes.length);
+    const memeToDisplay = memes[randomMemeId];
+    setRandomMeme(memeToDisplay);
+  }
+
+  //Handle button click
+  const onclick = (e) => {
+    e.preventDefault();
+    getRandomMeme();
+  };
+
   return (
     <div>
       <form action="" className="meme-form">
@@ -9,11 +45,14 @@ function MemeForm() {
           <input type="text" placeholder="Shut up" />
           <input type="text" placeholder="and take my money" />
         </div>
-        <button className="form-btn">
+        <button type="submit" className="form-btn" onClick={(e) => onclick(e)}>
           <span> Get a new meme image </span>
           <BsCardImage />
         </button>
       </form>
+      <div className="random-meme-img">
+        {randomMeme && <img src={randomMeme.url} alt="" />}
+      </div>
     </div>
   );
 }
